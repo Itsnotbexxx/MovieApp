1541dc9411449846c3bfa11537fde9df433b513e
Itsnotbexxx <nurpeis.beka@gmail.com>
2022-05-14 19:45:48 +0600

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Бексултан Нурпейс on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
